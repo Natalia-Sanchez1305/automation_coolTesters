5c8876b9b771bdd840710c6499e5f3c097bd9042
Jky0 <62363574+Jky0@users.noreply.github.com>
2020-07-25 11:56:57 -0500

    Ejemplo 1 Clase 20200725
    Descripcion Ejemplo 1 Clase 20200725

---
+++ automationCoolTesters/src/main/java/JuanEjerciciosSelenium/SeleniumEjemplo1.java
@@ -0,0 +1,68 @@
+package JuanEjerciciosSelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SeleniumEjemplo1 {
+	String url ="https://opensource-demo.orangehrmlive.com/";
+	WebDriver driver;
+	
+	@BeforeTest
+	public void startDriver() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--start--maximized");
+		options.addArguments("--incognito");
+		System.setProperty("webdriver.chrome.driver","chromedriver\\chromedriver.exe");
+		driver = new ChromeDriver(options);
+		driver.get(url);
+	}
+	
+	@Test(priority=1)
+	public void loginfail() {
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin12");
+		driver.findElement(By.id("btnLogin")).submit();		
+	}
+	
+	
+	
+	@Test(priority=2)
+	public void login() {
+		
+		driver.findElement(By.id("txtUsername")).clear();
+		driver.findElement(By.id("txtPassword")).clear();
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).submit();		
+	}
+	
+	@AfterTest
+	public void closeBrowser() {
+		driver.close();	
+	}
+	
+// para importar todas las librerias presionar Ctrl+ Shift + O
+//	public static void main(String[] args) {
+//		System.setProperty("webdriver.chrome.driver","chromedriver\\chromedriver.exe");
+//		//crear objeto ChromeDriver
+//		WebDriver driver = new ChromeDriver();
+//		driver.manage().window().maximize();
+//		driver.get("https://www.google.com/");
+//		driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
+//		//localizar el elemento de usuario para ingresarlo
+//		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+//		
+//		
+//		driver.findElement(By.xpath("//input[@id=\"txtUsername\"]")).clear();
+//		driver.findElement(By.xpath("//input[@id=\"txtUsername\"]")).sendKeys("Admin");
+//		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+//		driver.findElement(By.id("btnLogin")).submit();
+//		
+//		
+//	}
+}
