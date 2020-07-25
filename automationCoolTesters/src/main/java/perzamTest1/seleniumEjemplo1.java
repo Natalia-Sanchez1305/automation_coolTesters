6b0a9b89d1dc8ccb941ca14810c6877e1069afaa
Javier Perez <perzam@gmail.com>
2020-07-25 08:27:18 -0500

    test1
    test1

---
+++ automationCoolTesters/src/main/java/perzamTest1/seleniumEjemplo1.java
@@ -0,0 +1,10 @@
+package perzamTest1;
+
+public class seleniumEjemplo1 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "chromedriver\\chromedriver.exe");
+	} 
+	
+	
+}
