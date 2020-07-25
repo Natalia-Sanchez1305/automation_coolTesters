a24e289922ce9df24983602a81329d8aceabfd55
davidgfc87 <67280890+davidgfc87@users.noreply.github.com>
2020-07-25 11:56:00 -0500

    CambiosClase
    Ejercicio 07252020

---
+++ automationCoolTesters/src/main/java/sergioEjercicioSelenium/basicChromeDriverTest.java
@@ -0,0 +1,33 @@
+package sergioEjercicioSelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class basicChromeDriverTest {
+	
+	  public static void main(String[] args) { // TODO Auto-generated method stub
+	  System.setProperty("webdriver.chrome.driver",
+	  "chromedriver\\chromedriver.exe"); 
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("https://www.google.com");
+	  driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
+	  driver.manage().window().maximize();
+	  driver.get("https://opensource-demo.orangehrmlive.com/");
+	  driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+	  driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+	  driver.findElement(By.id("btnLogin")).click();
+	  //driver.findElement(By.id("btnLogin")).submit();
+	  
+	  //driver.findElement(By.id("txtUsername")).clear();
+	  
+	  //driver.findElement(By.name("txtUsername")).sendKeys("Admin2");
+	  //driver.findElement(By.name("txtUsername")).clear();
+	  
+	  //driver.findElement(By.xpath("//input[@id=\"txtUsername\"]")).sendKeys("Admin3");
+	  //driver.findElement(By.xpath("//input[@id=\"txtUsername\"]")).clear(); }
+	 
+
+	  }
+	  	  
+}
---
+++ automationCoolTesters/src/main/java/sergioEjercicioSelenium/seleniumEjemplo.java
@@ -0,0 +1,60 @@
+package sergioEjercicioSelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Reporter;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class seleniumEjemplo {
+	
+	String url = "https://opensource-demo.orangehrmlive.com/";
+	WebDriver driver;
+	String user, password;
+
+	@BeforeTest
+	public void startWebDriver() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--start-maximized");
+		options.addArguments("--incognito");
+		System.setProperty("webdriver.chrome.driver","chromedriver\\chromedriver.exe"); 
+		driver = new ChromeDriver(options);
+		driver.get(url);
+		user = "Admin";
+		password = "admin123";
+	}
+	
+	@Test(priority=1)
+	public void loginFail() {
+		WebElement txt_usuario = driver.findElement(By.id("txtUsername"));
+		txt_usuario.sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin1234");
+		driver.findElement(By.id("btnLogin")).submit();
+	}
+	
+	@Test(priority=2)
+	public void login() {
+		driver.findElement(By.id("txtUsername")).clear();
+		driver.findElement(By.id("txtPassword")).clear();
+		driver.findElement(By.id("txtUsername")).sendKeys(user);
+		Reporter.log("Usuario Ingresado" + "\n" + user + " " +  password, true);
+		driver.findElement(By.id("txtPassword")).sendKeys(password);
+		driver.findElement(By.id("btnLogin")).submit();
+	}
+	
+	@Test(priority=3)
+	public void logout() {
+		
+	}
+	
+	@AfterTest
+	public void closeBrowser() {
+		driver.close();
+	}
+	
+
+}
