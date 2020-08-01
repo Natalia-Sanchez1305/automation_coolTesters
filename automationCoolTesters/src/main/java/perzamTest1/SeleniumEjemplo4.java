8d78d5d53495ec8d92736d545f18b801c1689214
Javier Perez <perzam@gmail.com>
2020-08-01 10:13:24 -0500

    update
    Update

--- automationCoolTesters/src/main/java/perzamTest1/SeleniumEjemplo4.java
+++ automationCoolTesters/src/main/java/perzamTest1/SeleniumEjemplo4.java
@@ -1,8 +1,10 @@
 package perzamTest1;
 
 import java.time.Duration;
-import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
+
+ 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,8 +12,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.Reporter;
@@ -23,15 +23,16 @@ import org.testng.annotations.Test;
  * 2. Acceder correctamente al sistema (usuario:Admin password:admin123 ) 
  * 3. Validar que existen datos en la grafica del tab de Dashboard. 5. salir del sistema.
  */
-public class SeleniumEjemplo3 {
+public class SeleniumEjemplo4 {
     String url = "https://opensource-demo.orangehrmlive.com";
     String user="Admin";
     String password = "admin123";
-    WebDriver driver, wait;
+    WebDriver driver;
+    WebDriverWait wait;
     //elementos
     @FindBy(id="txtUsername")
     WebElement txt_usuario;
-    @FindBy(id="txtPassword")
+    @FindBy(name="txtPassword")
     WebElement txt_contraseña;
     @FindBy(id="btnLogin")
     WebElement btn_login;
@@ -51,7 +52,9 @@ public class SeleniumEjemplo3 {
         driver = new ChromeDriver(options);
         PageFactory.initElements(driver, this);
         driver.get(url);
-        driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS) ;
+        driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        
     }
     
     @Test(priority=1)
@@ -59,24 +62,22 @@ public class SeleniumEjemplo3 {
         txt_usuario.sendKeys("Admin");
         txt_contraseña.sendKeys("admin12");
         btn_login.submit();
-//        if(tbl_menu.isDisplayed()) {
-//        	Reporter.log("El login fue exitoso", true);
-//        	}
-//        else {
-//        	Reporter.log("El login NO fue exitoso", true);
-//        	}
-
-     
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        
+//        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+//                .withTimeout(Duration.ofSeconds(30))
+//                .pollingEvery(Duration.ofSeconds(7))
+//                .ignoring(NoSuchElementException.class);
+//                
+//        wait.until(ExpectedConditions.visibilityOf(tbl_menu));
+    
         wait.until(ExpectedConditions.visibilityOf(tbl_menu));
+        wait.until(ExpectedConditions.elementToBeClickable(tbl_menu));
         if(driver.getPageSource().contains("Dashboard")) {
-        	Reporter.log("El login fue exitoso", true);
+            Reporter.log("El login fue exitoso", true);
+        }else {
+            Assert.assertTrue(false, "El login No fue exitoso");
         }
-        else {
-        	Reporter.log("El login NO fue exitoso", true);
-        	Assert.assertFalse(true);
-        }
-        }
+    }
     
     @Test(priority=2)
     public void login() {
@@ -86,30 +87,29 @@ public class SeleniumEjemplo3 {
         Reporter.log("Usuario Ingresado: " + user , true);
         txt_contraseña.sendKeys(password);
         Reporter.log("Password Ingresado: " + password +"\n" , true);
-        btn_login.submit();
+        btn_login.click();
         Reporter.log("Click en Login\n", true);
-//        if(tbl_menu.isDisplayed()) {
-//        	Reporter.log("El login fue exitoso", true);
-//        	}
-//        else {
-//        	Reporter.log("El login NO fue exitoso", true);
-//        	}
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        
+    
         wait.until(ExpectedConditions.visibilityOf(tbl_menu));
-        if(driver.getPageSource().contains("Dashboard")) {
-        	Reporter.log("El login fue exitoso", true);
+        wait.until(ExpectedConditions.elementToBeClickable(tbl_menu));
+        
+        if(tbl_menu.isDisplayed()) {
+            Reporter.log("El login fue exitoso", true);
         }
-        else {
-        	Reporter.log("El login NO fue exitoso", true);
-        	Assert.assertFalse(true);
-        }
+        
+//        if(driver.getPageSource().contains("Dashboard")) {
+//            Reporter.log("El login fue exitoso", true);
+//            Assert.assertTrue(true, "El login fue exitoso");
+//        }
+        
     }
     
     @AfterTest
     public void closeBrowser() {
         btn_welcome.click();
         link_logout.click();
-        driver.close();
+//        driver.close();
     }
     
 
